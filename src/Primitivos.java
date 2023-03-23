6540c2fecd9076d4bfdfc8d2adf9a45249ed2d46
Marcos12UdeG <isaac.acosta@alumnos.udg.mx>
2023-03-22 23:59:02 -0600

    SE AGREGO

---
+++ src/Primitivos.java
@@ -0,0 +1,11 @@
+import java.lang.invoke.VarHandle;
+
+public class Primitivos {
+
+    public static void main(String[] args) {
+
+        float real = 2.12e3F;
+
+        System.out.println("real = " + real);
+    }
+}
